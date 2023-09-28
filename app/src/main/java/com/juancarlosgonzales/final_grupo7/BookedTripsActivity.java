6d16bbb35eb857a6bff8f571eac5deb03b3087ec
Juan Carlos <juancarlos@pattern.pe>
2023-09-27 19:46:52 -0500

    ajust

--- app/src/main/java/com/juancarlosgonzales/final_grupo7/BookedTripsActivity.java
+++ app/src/main/java/com/juancarlosgonzales/final_grupo7/BookedTripsActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.juancarlosgonzales.final_grupo7.entidades.Trip;
@@ -18,32 +21,65 @@ public class BookedTripsActivity extends AppCompatActivity {
 
     DAOTrip daoTrip = new DAOTrip(this);
     List<Trip> tripList = new ArrayList<>();
+    List<Trip> filteredList = new ArrayList<>();
     TripAdapter adapter;
     RecyclerView rvTrips;
     FloatingActionButton btnNew;
+    EditText etSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booked_trips);
+
         assignReferences();
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
         displayTrips();
     }
 
-    private void assignReferences(){
+    private void assignReferences() {
         rvTrips = findViewById(R.id.rvTrips);
         btnNew = findViewById(R.id.btnNew);
+        etSearch = findViewById(R.id.etSearch);
+
         btnNew.setOnClickListener(v -> {
             Intent intent = new Intent(this, BookTripActivity.class);
             startActivity(intent);
         });
     }
 
-    private void displayTrips(){
+    private void displayTrips() {
         daoTrip.grantDatabaseAccess();
         tripList = daoTrip.loadTrips();
-        adapter = new TripAdapter(this, tripList);
+        filteredList.addAll(tripList);
+        adapter = new TripAdapter(this, filteredList);
         rvTrips.setAdapter(adapter);
         rvTrips.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    private void filter(String text) {
+        filteredList.clear();
+        for (Trip item : tripList) {
+            if (item.getTren().toLowerCase().contains(text.toLowerCase())
+                    || item.getMetropolitano().toLowerCase().contains(text.toLowerCase())
+                    || item.getCorredor().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+        adapter.notifyDataSetChanged();
+    }
+
 }
